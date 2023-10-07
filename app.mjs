1bd15b18b0847c369cdae1b1f74359d340c57fc8
안혜영 <hyahn@emart.com>
2023-10-07 14:24:11 +0900

    db connection 추가

--- app.mjs
+++ app.mjs
@@ -1,3 +1,17 @@
+const { Client } = require("pg");
+
+
+const cli = new Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: 5432
+});
+
+cli.connect();
+console.log(`${cli.user ? cli.user : "None"}`)
+
 export const handler = async (event) => {
   // TODO implement
   console.log(event)
